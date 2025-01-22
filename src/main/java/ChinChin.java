4770796504b2c1743252326753a1cc1e5828b9f2
junlin <117089326+chanjunlin@users.noreply.github.com>
2025-01-22 21:24:19 +0800

    Echo

--- src/main/java/ChinChin.java
+++ src/main/java/ChinChin.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class ChinChin {
     public static void main(String[] args) {
+
+        // Constants
+        Scanner scanner = new Scanner(System.in);
+
         String line = "-----------------------------";
-        String greetings = "Hello, I am ChinChin\nWhat can I do for you?\n";
-        String goodbyes = "\nBye! Hope to see you again soon!\n";
-        System.out.println(line + "\n" + greetings + line + goodbyes + line);
+        String greetings = "Hello, I am ChinChin\nWhat can I do for you?";
+        String goodbyes = "Bye. Hope to see you again soon!";
+
+        // Friendly greetings
+        System.out.println(line);
+        System.out.println(greetings);
+        System.out.println(line + "\n");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.toLowerCase().equals("bye")) {
+                System.out.println("\n" + line);
+                System.out.println(goodbyes);
+                System.out.println(line + "\n");
+                break;
+            } else {
+                System.out.println("\n" + line);
+                System.out.println(userInput);
+                System.out.println(line + "\n");
+            }
+        }
     }
 }
