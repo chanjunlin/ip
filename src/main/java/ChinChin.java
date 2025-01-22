4b523c241acfc5da0a08bc07486d9ded52cffaf9
junlin <117089326+chanjunlin@users.noreply.github.com>
2025-01-22 20:18:57 +0800

    Changed bot name

---
+++ src/main/java/ChinChin.java
@@ -0,0 +1,8 @@
+public class ChinChin {
+    public static void main(String[] args) {
+        String line = "-----------------------------";
+        String greetings = "Hello, I am ChinChin\nWhat can I do for you?\n";
+        String goodbyes = "\nBye! Hope to see you again soon!\n";
+        System.out.println(line + "\n" + greetings + line + goodbyes + line);
+    }
+}
